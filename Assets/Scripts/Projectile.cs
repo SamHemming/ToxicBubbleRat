08947767c6cd7df0c8fe399eefc9a8876e2678a1
SamHemming <32447304+SamHemming@users.noreply.github.com>
2025-04-16 18:26:51 +0300

    Animation for luu

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ProjectileType{ Normal, Electric }
+
 [RequireComponent(typeof(SpriteRenderer), typeof(CircleCollider2D), typeof(Rigidbody2D))]
 public class Projectile : MonoBehaviour
 {
@@ -10,31 +12,68 @@ public class Projectile : MonoBehaviour
 	public float rotationSpeed = 1f;
 	public float lifeSpan = 1f;
 	public Vector2 heading = Vector2.zero;
+	public ProjectileType projectileType = ProjectileType.Normal;
 
 	private bool isSpent = false;
+	private bool inPosition = false;
+	private Coroutine coroutine = null;
+	private float timer = 0f;
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if(isSpent) return;
+		if (isSpent) return;
 		Enemy target = collision.collider.GetComponent<Enemy>();
 		if (target == null) return; //didn't hit enemy
 
 		target.Damage(damage);
-		isSpent = true;
-
-		Destroy(this.gameObject);
+		if (projectileType == ProjectileType.Normal)
+		{
+			isSpent = true;
+			Destroy(this.gameObject);
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		lifeSpan -= Time.deltaTime;
-		if (lifeSpan <= 0) //time is up bitch
+		if (projectileType == ProjectileType.Normal)
 		{
-			Destroy(this.gameObject);
+			lifeSpan -= Time.deltaTime;
+			if (lifeSpan <= 0) //time is up bitch
+			{
+				Destroy(this.gameObject);
+			}
+
+			transform.position += (Vector3)heading * speed * Time.deltaTime; //move
+
+			transform.Rotate(0, 0, rotationSpeed + Time.deltaTime); //rotate around Z-axis
 		}
+		else if (projectileType == ProjectileType.Electric)
+		{
+			if (!inPosition)
+			{
+				if (coroutine == null)
+				{
+					coroutine = StartCoroutine(Move());
+				}
+			return;
+			}
 
-		transform.position += (Vector3)heading * speed * Time.deltaTime; //move
+			timer += Time.fixedDeltaTime;
 
-		transform.Rotate(0, 0, rotationSpeed + Time.deltaTime); //rotate around Z-axis
+			if (timer > lifeSpan)
+			{
+				Destroy(this.gameObject);
+			}
+
+		}
 	}
+
+	private IEnumerator Move()
+	{
+		//Where to go?????
+		yield return null;
+	}
+
+
+
 }
